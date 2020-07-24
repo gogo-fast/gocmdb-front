cc38942011b20bc5f2b6c481a187f3bac449a0b0
gogo-fast <scm.scm.scm@qq.com>
2020-07-24 08:38:36 +0800

    change api urls

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,13 @@
 // url of nginx proxy, use api prefix,
 // and been write off while request from nginx to api server
-// const apiUrl = 'http://go.cmdb.com:8888/api/v1';
-// const apiWsUrl = 'ws://go.cmdb.com:8888/api/v1';
-// const imgUrl = 'http://go.cmdb.com:8888/api';
+const apiUrl = 'http://go.cmdb.com:8888/api/v1';
+const apiWsUrl = 'ws://go.cmdb.com:8888/api/v1';
+const imgUrl = 'http://go.cmdb.com:8888/api';
 
 // used for dev server
-const apiUrl = 'http://go.cmdb.com:8000/v1';
-const apiWsUrl = 'ws://go.cmdb.com:8000/v1';
-const imgUrl = 'http://go.cmdb.com:8000';
+// const apiUrl = 'http://go.cmdb.com:8000/v1';
+// const apiWsUrl = 'ws://go.cmdb.com:8000/v1';
+// const imgUrl = 'http://go.cmdb.com:8000';
 
 const pageSizeOptions = ["5", "10", "20", "50"];
 
